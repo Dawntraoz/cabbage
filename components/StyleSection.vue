5311f008ff7526b7b1f538b9bb34fba845870704
albasilventefuentes <alba@square1.io>
2020-05-17 14:54:01 +0200

    Dynamic components by Styleguide section

--- components/StyleSection.vue
+++ components/StyleSection.vue
@@ -2,20 +2,24 @@
   <div class="pt-8">
     <header class="pb-6">
       <h3 class="capitalize">
-        <slot name="title"></slot>
+        {{ blok.title }}
       </h3>
       <p class="pb-2 border-b border-gray-300 text-gray-700">
-        <slot name="title"></slot> for app theme.
+        {{ blok.title }} for app theme.
       </p>
     </header>
+    <component
+      :is="blok.component"
+      v-for="blok in blok.body"
+      :key="blok._uid"
+      :blok="blok"
+    ></component>
     <slot />
   </div>
 </template>
 
 <script>
 export default {
-  components: {}
+  props: ['blok']
 }
 </script>
-
-<style scoped></style>
