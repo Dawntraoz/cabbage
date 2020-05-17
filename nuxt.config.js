7af5c10e90bf3d5e2198dbfd6aa46353115d7edc
albasilventefuentes <alba@square1.io>
2020-05-17 13:23:52 +0200

    Create Page Content Type and dynamic routes with Storyblok & VueJS dynamic components

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ module.exports = {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: [],
+  plugins: ['~/plugins/components.js'],
   /*
    ** Nuxt.js dev-modules
    */
---
+++ plugins/components.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import Page from '@/components/Page.vue'
+
+Vue.component('page', Page)
