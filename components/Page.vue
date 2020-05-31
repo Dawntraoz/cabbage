377a600070f2d4ce123cd131e02ae4c5ac658d08
albasilventefuentes <alba@square1.io>
2020-05-31 19:46:47 +0200

    Changes on the main layout

--- components/Page.vue
+++ components/Page.vue
@@ -1,16 +1,31 @@
 <template>
-  <div>
-    <component
-      :is="blok.component"
-      v-for="blok in blok.body"
-      :key="blok._uid"
-      :blok="blok"
-    ></component>
+  <div class="flex flex-wrap lg:items-start h-screen overflow-y-auto">
+    <div
+      class="w-full lg:w-3/4 lg:h-screen lg:overflow-y-auto flex flex-wrap p-4 pb-12 md:pt-6 md:px-10 "
+    >
+      <header>
+        <h1>{{ name }}</h1>
+      </header>
+      <component
+        :is="blok.component"
+        v-for="blok in blok.body"
+        :key="blok._uid"
+        :blok="blok"
+      ></component>
+    </div>
+    <div class="w-full lg:w-1/4 lg:h-full bg-white p-4 pb-12 lg:pt-6 lg:px-10">
+      <component
+        :is="blok.component"
+        v-for="blok in blok.sidebar"
+        :key="blok._uid"
+        :blok="blok"
+      ></component>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['blok']
+  props: ['name', 'blok']
 }
 </script>
